0f488375a9ee73a8a6782a0891d46e6fba4a50b4
frobbins <robbinsfatim@gmail.com>
2020-11-27 18:22:18 -0700

    Added mongoose schema

---
+++ Develop/models/index.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+    {
+      toJSON: {
+      
+        virtuals: true,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+workoutSchema.virtual("totalDuration").get(function () {
+
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+module.exports = Workout;
